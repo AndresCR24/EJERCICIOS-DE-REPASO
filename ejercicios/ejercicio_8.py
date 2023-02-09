a81a204b35b936bff4375e1c423b690c9dda87a3
AndresCR24 <c-andres-98@hotmail.com>
2023-02-09 18:57:01 -0500

    Se hace el commit del ejercicio faltante 8

---
+++ ejercicios/ejercicio_8.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+
+    lista = [1, 2, 3, 4, 5, 6 ,7, 8, 9, 10]
+
+    def invertir_lista(lista):
+        
+      lista = lista [::-1]
+      return lista
+
+    print(invertir_lista(lista))
