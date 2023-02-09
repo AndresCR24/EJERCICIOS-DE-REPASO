28743acee3685f30fb851d4f4e456a4b542786b6
AndresCR24 <c-andres-98@hotmail.com>
2023-02-09 17:53:34 -0500

    se hace commit del ejercicio 13

---
+++ ejercicios/ejercicio_13.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+
+    numero_1 = float(input("Ingrese un numero: "))
+    numero_2 = float(input("Ingrese un segundo numero: "))
+
+    suma = numero_1 + numero_2
+    resta = numero_1 - numero_2
+    multiplicacion = numero_1 * numero_2
+    division = numero_1 / numero_2
+
+    print(f"El resultado de sumar tus dos numeros es {suma}")
+    print(f"El resultado de restar tus dos numeros es {resta}")
+    print(f"El resultado de multiplicar tus dos numeros es {multiplicacion}")
+    print(f"El resultado de dividir tus dos numeros es {division}")
