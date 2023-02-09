b163a2b82a77c1d090dbfb410e2c6e7340f70804
AndresCR24 <c-andres-98@hotmail.com>
2023-02-09 11:09:36 -0500

    Se hace el commit inicial con el ejerciccio numero 1

---
+++ app.py
@@ -0,0 +1,1 @@
+import ejercicios.ejecicio_1
---
+++ ejercicios/ejecicio_1.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    nombre = input("Ingresa tu nombre")
+    edad = input("Ingresa tu edad")
+
+    print("tu nombre es "+nombre+"y tienes "+edad+" años")
