c11d4e9f53d41ff207cd11e8e06e6601bc040c97
AndresCR24 <c-andres-98@hotmail.com>
2023-02-09 11:40:49 -0500

    Se hace el cuarto commit con el ejercicio numero 4

---
+++ ejercicios/ejercicio_4.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    numero_ingresado = int(input("Ingrese un numero: "))
+
+    if numero_ingresado % 2 == 0:
+        print("El numero es par ")
+
+    else:
+        print("El numero es impar")
